a9281d79b9d5ae6021a23aa26bd922d289d948f7
navjotSingh2000 <abcnavjot2000@gmail.com>
2025-02-08 16:55:45 -0500

    feat: added tailwindcss

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Tab() {
   return (
-    <View style={styles.container}>
+    <View className="flex justify-center items-center">
       <Text>Tab Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "../global.css";
 import { Stack } from "expo-router/stack";
 
 export default function Layout() {
